4f690f8f32f7c474d44cc6c7dc2ec5a6ed17fb11
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-06-17 16:34:06 +0200

    page projet

--- projet.php
+++ projet.php
@@ -9,30 +9,82 @@
 
      <body class="background-projet">
 
+      <?php include "include/navigation.php"; ?>
+
+  <h1 class="title-projet bold size-title">Projets réalisés lors de la formation Descodeuses</h1>
 
 
-       <?php include "include/navigation.php"; ?>
+<!--------------- BLOCK PROJET CODEVORES -- ---------------->
 
-  <h1 class="title-projet">Projets réalisés lors de la formation Descodeuses</h1>
+  <div class="container block-codevores bold">
+    <h2 class="text-center size-title pb-3 pt-2 size-title">Codevores</h2>
 
-<div class="block-projet container">
+    <!-- TEXTE -->
+      <div class="row col-10 d-flex justify-content-center text-center ml-5 pb-3">
+          <p class="pb-2">Thème: recrutement, mise en relation entre les professionnels de la technologie et les potentiels recruteurs.</p>
+          <p>Technologies utilisées: Html, Css3, JavaScript.</p>
+      </div>
+      <!-- IMAGE -->
+      <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+          <img src="assets/img/img_codevores.png" alt="" class="img_site-recrutement">
+      </div>
 
-  <div class="block-codevores">
-    <a href="projets/SITE-CODEVORES">Codevores</a>
+      <!-- BOUTON -->
+    <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+        <button type="button" name="button" class="btn-projet btn btn-light"><a href="projets/SITE-CODEVORES">Codevores</a></button>
+    </div>
+
   </div>
 
-  <div class="block-hotel">
-      <a href="projets/Ada_Hotel_New">Ada Hotel</a>
-  </div>
 
 
-  <div class="block-musee">
-      <a href="projets/musee">Arts Paname</a>
-  </div>
+  <!---------------- BLOCK PROJET ADA HOTEL -- ---------------->
+
+    <div class="container block-codevores bold">
+      <h2 class="text-center size-title pb-3 pt-2"> Ada Hotel</h2>
+
+      <!-- TEXTE -->
+        <div class="row col-10 d-flex justify-content-center text-center ml-5 pb-3">
+            <p class="pb-2">Thème: Hotel, offrant la possibilité de réservation.</p>
+            <p>Technologies utilisées: Html, Css3, JavaScript.</p>
+        </div>
+        <!-- IMAGE -->
+        <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+            <img src="assets/img/ada_hotel.png" alt="" class="img_site-recrutement">
+        </div>
+
+        <!-- BOUTON -->
+      <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+          <button type="button" name="button" class="btn-projet btn btn-light"><a href="projets/SITE-CODEVORES">Ada Hotel</a></button>
+      </div>
+
+    </div>
 
 
 
-</div>
+
+      <!---------------- BLOCK PROJET ADA HOTEL -- ---------------->
+
+        <div class="container block-codevores bold">
+          <h2 class="text-center size-title pb-3 pt-2"> Musée</h2>
+
+          <!-- TEXTE -->
+            <div class="row col-10 d-flex justify-content-center text-center ml-5 pb-3">
+                <p class="pb-2">Thème: Musée.</p>
+                <p>Technologies utilisées: PhP, Html, Css3.</p>
+            </div>
+            <!-- IMAGE -->
+            <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+                  <img src="assets/img/musee_picture.png" alt="" class="img_site-recrutement">
+            </div>
+
+            <!-- BOUTON -->
+          <div class="row col-10 d-flex justify-content-center ml-5 pb-3">
+              <button type="button" name="button" class="btn-projet btn btn-light"><a href="projets/SITE-CODEVORES">Arts Paname</a></button>
+          </div>
+
+        </div>
+
 
 
 
